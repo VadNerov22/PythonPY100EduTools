7ab55f6dc7a6afe8c563f1509a8bbe89fc403a7e
VadNerov22 <vadoleg@mail.ru>
2023-03-20 21:52:18 +0300

    Python2023_SPB_M1

--- Тема 2/Составные типы данных/task8/main.py
+++ Тема 2/Составные типы данных/task8/main.py
@@ -1,3 +1,4 @@
 str_ = "sdkfjsdkjfnksjdnfkjsdnfkjsndfkjsndfkjsndfkjsnd"
-
+count_ = len(set(str_)) # количество уникальных символов в строке
 # TODO найти и распечатать количество уникальных символов в строке
+print(count_)

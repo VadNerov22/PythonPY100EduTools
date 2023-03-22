48c7d070823cef83dfb1e9b666d221dd81fdc303
VadNerov22 <vadoleg@mail.ru>
2023-03-22 23:23:03 +0300

    initial commit

--- Тема 3/Циклы for и while/task5/main.py
+++ Тема 3/Циклы for и while/task5/main.py
@@ -1,3 +1,4 @@
 list_students = ["Маша", "Петя", "Саша", "Кирилл", "Оля"]
 
-# TODO распечатать в столбик все имена
+for student in list_students:
+    print(student)
--- Тема 3/Циклы for и while/task9/main.py
+++ Тема 3/Циклы for и while/task9/main.py
@@ -26,4 +26,5 @@ surname_list = [  # список фамилий Санкт-Петербурга
     "Соловьёв"
 ]
 
-# TODO распечатать фамилии и их номера
+for pos, surname in enumerate(surname_list, start=1):
+    print(pos, surname)

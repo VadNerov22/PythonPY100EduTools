b75e22b7fa1b00cc09948f40f49ddd3894ce2061
VadNerov22 <vadoleg@mail.ru>
2023-04-06 07:26:22 +0300

    Выполненная ЛР 4.

--- Тема 5/Лабораторная работа/task1/main.py
+++ Тема 5/Лабораторная работа/task1/main.py
@@ -1,1 +1,5 @@
-# TODO решить с помощью list comprehension и распечатать его
+from pprint import pprint
+
+dic = [{n: v for n, v in zip(["bin", "dec", "hex", "oct"], [bin(i), i, hex(i), oct(i)])} for i in range(16)]
+
+pprint(dic)

3ab2343e7b09342b0afdbc780d6772bfa75139e1
VadNerov22 <127225856+VadNerov22@users.noreply.github.com>
2025-11-21 12:53:50 +0300

    Update main.py

--- Тема 5/Лабораторная работа/task5/main.py
+++ Тема 5/Лабораторная работа/task5/main.py
@@ -1,9 +1,10 @@
 import string
 import random
-def get_random_password(n: int=8) -> str:
-    symbol_parol = list(string.ascii_uppercase + string.ascii_lowercase + string.digits)
-    parol = "".join(random.sample(symbol_parol, n))
 
-    return parol
+
+def get_random_password(n: int=8) -> str:
+    symbol_parol = string.ascii_letters + string.digits
+    return "".join(random.sample(symbol_parol, n))
+
 
 print(get_random_password())
